5c765bea1df97feba4365f1c88967a5eb4dd83d4
metalex9 <alexbainter@gmail.com>
2017-02-06 21:04:38 -0600

    Add site.scss to app.

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
-import './testing.scss';
+import 'normalize.css';
+import './styles/site.scss';
 
 ReactDOM.render(
     <Router history={browserHistory} routes={routes} />
