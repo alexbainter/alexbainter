54cb679eaddba7e7fd8418b047e2dfdcc2bde2b7
metalex9 <alexbainter@gmail.com>
2017-02-06 21:02:12 -0600

    Add CSS loader to webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,13 @@ module.exports = {
                     fallback: 'style-loader',
                     use: 'css-loader!sass-loader',
               }),
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: 'css-loader'
+                })
             }
         ]
     },
