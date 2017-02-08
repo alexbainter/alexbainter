ba5b47db54e2fec1b5dd5977883447cca492a8a4
metalex9 <alexbainter@gmail.com>
2017-02-07 21:21:27 -0600

    Add code writing thing on the homepage.

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,9 +1,57 @@
 import React, { Component } from 'react';
+import '../styles/_home.scss';
 
 export default class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { code: '' };
+        this.typeCode(testCode);
+    }
+    render() {
+        return (
+            <div>
+                <pre><code>{this.state.code}</code></pre>
+            </div>
+        );
+    }
+
+    typeCode(text) {
+        let textIndex = 0;
+        let typeInterval = setInterval(() => {
+            if (textIndex < text.length) {
+                this.setState({code: this.state.code += text[textIndex++]});
+            } else {
+                clearInterval(typeInterval);
+            }
+        }, 40);
+    }
+}
+
+const testCode = `import React, { Component } from 'react';
+import '../styles/_home.scss';
+
+export default class Home extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { code: '' };
+        const testCode = 'Beep boop';
+        this.typeCode(testCode);
+    }
     render() {
         return (
             <div className="code">{this.state.code}</div>
         );
     }
-}
+
+    typeCode(text) {
+        let textIndex = 0;
+        let typeInterval = setInterval(() => {
+            if (textIndex < text.length) {
+                this.setState({code: this.state.code += text[textIndex++]});
+            } else {
+                clearInterval(typeInterval);
+            }
+        }, 85);
+    }
+}`
