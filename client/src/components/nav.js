3c75a63cc6b47e16d9650c3719fadf57f4063d1f
metalex9 <alexbainter@gmail.com>
2017-02-08 19:40:03 -0600

    Style nav links.

--- client/src/components/nav.js
+++ client/src/components/nav.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router';
+import { Link, IndexLink } from 'react-router';
 import '../styles/_nav.scss';
 
 export default class Nav extends Component {
     render() {
         return (
             <div className="nav">
-                <Link to="/" className="nav__header">
+                <IndexLink to="/" className="nav__header" activeClassName="nav__header--active">
                     alexbainter
-                </Link>
-                <Link to="/skills" className="nav__link">
+                </IndexLink>
+                <Link to="/skills" className="nav__link" activeClassName="nav__link--active">
                     skills
                 </Link>
-                <Link to="/work" className="nav__link">
+                <Link to="/work" className="nav__link" activeClassName="nav__link--active">
                     work
                 </Link>
-                <Link to="/about" className="nav__link">
+                <Link to="/about" className="nav__link" activeClassName="nav__link--active">
                     about
                 </Link>
             </div>
