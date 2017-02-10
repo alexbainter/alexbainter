dc73b5e6ca85df11532d2e8f1ceb98f6f9560e45
metalex9 <alexbainter@gmail.com>
2017-02-09 20:57:33 -0600

    Add separate file for fake code snippets.

---
+++ client/src/components/fake-code-snippets.js
@@ -0,0 +1,13 @@
+export default [
+    `<!DOCTYPE html>
+    <html>
+    \t<head>
+    \t\t<title>alexbainter.com</title>
+    \t\t<link rel="stylesheet" href="/dist/site.css">
+    \t\t</head>
+    \t<body>
+    \t\t<div class="container"></div>
+    \t\t<script src="/dist/bundle.js"></script>
+    \t</body>
+    </html>`
+];
