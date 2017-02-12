5869667e2cc9cdbbcf3beee54f76ec15bfc96c56
metalex9 <alexbainter@gmail.com>
2017-02-12 15:28:51 -0600

    Update about component with some real content.

--- client/src/components/about.js
+++ client/src/components/about.js
@@ -2,6 +2,11 @@ import React from 'react';
 
 export default () => {
     return (
-        <div>Lorem ipsum</div>
+        <div>
+            Hey! My name's Alex. I'm a software developer. Everyone knows software developers need their own websites, and this one is mine.
+            <br />
+            <br />
+            If you'd like to contact me, you can do so at <a href="mailto:alex@alexbainter.com?Subject=Hello!">alex@alexbainter.com</a>.
+        </div>
     );
 }
