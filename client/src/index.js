fe086ec77f6f87c7fa3dfa2dd909a017d78ae322
metalex9 <alexbainter@gmail.com>
2017-02-15 20:14:22 -0600

    Load font-awesome.

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 import 'normalize.css';
-import './styles/site.scss';
+import 'font-awesome/scss/font-awesome.scss';
 
 ReactDOM.render(
     <Router history={browserHistory} routes={routes} />
