c9e6e61518e3700c969245a3e452c94a1f673d04
metalex9 <alexbainter@gmail.com>
2017-02-16 21:44:00 -0600

    Add skills page.

--- client/src/components/skills.js
+++ client/src/components/skills.js
@@ -5,8 +5,23 @@ export default class Skills extends Component {
     render() {
         return (
             <div>
-                <input className="skill-search" type="text" placeholder="JavaScript, C#, git"/>
+                <input className="skill-search" type="text" placeholder="JavaScript, C#, git" ref={(input) => { this.searchInput = input; }}/>
+                <ul className="skills-list">
+                    <li className="skills-list__item">
+                        oeu
+                    </li>
+                    <li className="skills-list__item">
+oeu
+                    </li>
+                    <li className="skills-list__item">
+oeu
+                    </li>
+                </ul>
             </div>
         )
+    }
+
+    componentDidMount() {
+        this.searchInput.focus();
     }
 }
