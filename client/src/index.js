f678b9f4eb64b3cdd5bccca9ed821ed7c7f9fab9
metalex9 <alexbainter@gmail.com>
2017-02-18 14:13:40 -0600

    Add in redux.

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import promise from 'redux-promise';
+import reducers from './reducers';
 import routes from './routes';
 import 'normalize.css';
 import 'font-awesome/scss/font-awesome.scss';
 
+const createStoreWithMiddleware = applyMiddleware(
+    promise
+)(createStore);
+
 ReactDOM.render(
-    <Router history={browserHistory} routes={routes} />
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <Router history={browserHistory} routes={routes} />
+    </Provider>
 , document.querySelector('.container'));
