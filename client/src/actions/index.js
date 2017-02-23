72fc076cf85417337b17bd015225e6d8359cb540
metalex9 <alexbainter@gmail.com>
2017-02-22 20:18:23 -0600

    Add actions for managing snippets.

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,6 +4,16 @@ export const FETCH_SKILLS = 'FETCH_SKILLS';
 export const FETCH_RATINGS = 'FETCH_RATINGS';
 export const FETCH_POSITIONS = 'FETCH_POSITIONS';
 export const FETCH_PROJECTS = 'FETCH_PROJECTS';
+export const FETCH_NEW_SNIPPET = 'FETCH_NEW_SNIPPET';
+export const CHANGE_SNIPPET = 'CHANGE_SNIPPET';
+
+export function fetchSkills() {
+    return baseAPIFetch(FETCH_SKILLS, 'skills');
+}
+
+export function fetchRatings() {
+    return baseAPIFetch(FETCH_RATINGS, 'ratings');
+}
 
 function baseAPIFetch(actionType, apiRoute) {
     const request = axios.get(`/api/${apiRoute}`);
@@ -13,9 +23,16 @@ function baseAPIFetch(actionType, apiRoute) {
     }
 }
 
-export function fetchSkills() {
-    return baseAPIFetch(FETCH_SKILLS, 'skills');
+export function fetchNewSnippet(currentSnippetId) {
+    const request = axios.post(`/api/snippet`, { currentSnippetId });
+    return {
+        type: FETCH_NEW_SNIPPET,
+        payload: request
+    }
 }
-export function fetchRatings() {
-    return baseAPIFetch(FETCH_RATINGS, 'ratings');
+
+export function changeSnippet() {
+    return {
+        type: CHANGE_SNIPPET
+    }
 }
