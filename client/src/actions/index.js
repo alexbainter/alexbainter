6f40b28a106ede11a26e86ea75d711d16381cce5
metalex9 <alexbainter@gmail.com>
2017-02-23 20:15:56 -0600

    Remove actions for fetching and changing snippets.

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,8 +4,6 @@ export const FETCH_SKILLS = 'FETCH_SKILLS';
 export const FETCH_RATINGS = 'FETCH_RATINGS';
 export const FETCH_POSITIONS = 'FETCH_POSITIONS';
 export const FETCH_PROJECTS = 'FETCH_PROJECTS';
-export const FETCH_NEW_SNIPPET = 'FETCH_NEW_SNIPPET';
-export const CHANGE_SNIPPET = 'CHANGE_SNIPPET';
 
 export function fetchSkills() {
     return baseAPIFetch(FETCH_SKILLS, 'skills');
@@ -20,19 +18,5 @@ function baseAPIFetch(actionType, apiRoute) {
     return {
         type: actionType,
         payload: request
-    }
-}
-
-export function fetchNewSnippet(currentSnippetId) {
-    const request = axios.post(`/api/snippet`, { currentSnippetId });
-    return {
-        type: FETCH_NEW_SNIPPET,
-        payload: request
-    }
-}
-
-export function changeSnippet() {
-    return {
-        type: CHANGE_SNIPPET
     }
 }
