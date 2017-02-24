9b167aabb004886b6e61a5aef88cab43b90c6741
metalex9 <alexbainter@gmail.com>
2017-02-23 20:17:13 -0600

    Format snippets properly in the seed file.

--- seed/snippets.js
+++ seed/snippets.js
@@ -1,68 +1,68 @@
 const Snippet = require('../models/Snippet');
 
 const data = [
-    `<!DOCTYPE html>
-    <html>
-        <head>
-            <title>alexbainter.com</title>
-            <link rel="stylesheet" href="/dist/site.css">
-        </head>
-        <body>
-            <div class="container"></div>
-            <script src="/dist/bundle.js"></script>
-        </body>
-    </html>`,
-    `import React, { Component } from 'react';
-    import Nav from './nav';
-    import '../styles/_app.scss';
+`<!DOCTYPE html>
+<html>
+    <head>
+        <title>alexbainter.com</title>
+        <link rel="stylesheet" href="/dist/site.css">
+    </head>
+    <body>
+        <div class="container"></div>
+        <script src="/dist/bundle.js"></script>
+    </body>
+</html>`,
+`import React, { Component } from 'react';
+import Nav from './nav';
+import '../styles/_app.scss';
 
-    export default class App extends Component {
-        render() {
-            return (
-                <div>
-                    <Nav />
-                    <div className="content">
-                        {this.props.children}
-                    </div>
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <Nav />
+                <div className="content">
+                    {this.props.children}
                 </div>
-            );
-        }
-    };`,
-    `$fakeCodeColor: black;
+            </div>
+        );
+    }
+};`,
+`$fakeCodeColor: black;
 
-    .fake-code {
-        font-family: 'Roboto Mono', monospace;
-        white-space: pre-wrap;
-        display: inline;
-        tab-size: 4;
-        font-size: 0.75em;
+.fake-code {
+    font-family: 'Roboto Mono', monospace;
+    white-space: pre-wrap;
+    display: inline;
+    tab-size: 4;
+    font-size: 0.75em;
 
-        &--selected {
-            background: $fakeCodeColor;
-            color: white;
-
-            &::after {
-                display: none;
-            }
-        }
-
-        &--idle {
-
-            &::after {
-                animation: 1s blink step-end infinite;
-            }
-        }
+    &--selected {
+        background: $fakeCodeColor;
+        color: white;
 
         &::after {
-            content: '';
-            display:inline-block;
-            vertical-align: middle;
-            height: 1em;
-            width: 2px;
-            background: $fakeCodeColor;
-            transform: translateY(-5%);
+            display: none;
         }
-    }`
+    }
+
+    &--idle {
+
+        &::after {
+            animation: 1s blink step-end infinite;
+        }
+    }
+
+    &::after {
+        content: '';
+        display:inline-block;
+        vertical-align: middle;
+        height: 1em;
+        width: 2px;
+        background: $fakeCodeColor;
+        transform: translateY(-5%);
+    }
+}`
 ];
 
 module.exports = Array.prototype.map.bind(data, code => {
