a38d9e8d1177296a3dd1bba1113639b88360b089
metalex9 <alexbainter@gmail.com>
2017-02-23 20:16:29 -0600

    Remove snippets reducer.

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import data from './data.reducer';
-import snippets from './snippets.reducer';
 
 const rootReducer = combineReducers({
-    data,
-    snippets
+    data
 });
 
 export default rootReducer;
