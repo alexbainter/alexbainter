f6da5ff296adf248ba81e5b9876985f0129053bc
metalex9 <alexbainter@gmail.com>
2017-02-26 12:23:43 -0600

    Add route for the work component.

--- client/src/routes.js
+++ client/src/routes.js
@@ -4,11 +4,13 @@ import App from './components/app';
 import About from './components/about';
 import Home from './components/home';
 import Skills from './components/skills';
+import Work from './components/work';
 
 export default (
     <Route path="/" component={App}>
         <IndexRoute component={Home} />
         <Route path="about" component={About} />
         <Route path="skills" component={Skills} />
+        <Route path="work" component={Work} />
     </Route>
 );
