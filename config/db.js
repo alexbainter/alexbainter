41e63de8086973388536f94b80b38de740b3cefd
metalex9 <alexbainter@gmail.com>
2017-02-26 16:48:06 -0600

    Get production connection string from an environment variable.

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-    path: process.env.NODE_ENV === 'production' ? '' : 'mongodb://localhost/alexbainter',
+    path: process.env.NODE_ENV === 'production' ? process.env.CONNECTION_STRING : 'mongodb://localhost/alexbainter',
     keepAliveMS: 1000
 }
