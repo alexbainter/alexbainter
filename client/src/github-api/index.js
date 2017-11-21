2fd48137213e36f1edeecd61917b9e22b00a1bd2
metalex9 <alexbainter@gmail.com>
2017-11-20 22:42:21 -0600

    Swap mem for lodash's memoize function.

--- client/src/github-api/index.js
+++ client/src/github-api/index.js
@@ -1,31 +1,40 @@
 import fileExtension from 'file-extension';
 import axios from 'axios';
 import flatten from 'array-flatten';
-import mem from 'mem';
+import { memoize as mem } from 'lodash';
 
 const acceptedFileTypes = ['js', 'html', 'scss'];
 
 const makeFilterByType = match => ({ type }) => type === match;
 
-const getContents = mem(async (path = '') => {
-  const { data } = await axios.get(
-    `https://api.github.com/repos/metalex9/alexbainter/contents${path}`
-  );
-  const files = data
-    .filter(makeFilterByType('file'))
-    .filter(({ name }) => acceptedFileTypes.includes(fileExtension(name)));
-  const dirs = data.filter(makeFilterByType('dir'));
-  const dirContents = await Promise.all(dirs.map(dir => getContents(dir.path)));
-  return flatten(files.concat(dirContents));
-});
+const getContents = mem(
+  (path = '') =>
+    new Promise((resolve, reject) => {
+      axios
+        .get(
+          `https://api.github.com/repos/metalex9/alexbainter/contents${path}`
+        )
+        .then(({ data }) => {
+          const files = data
+            .filter(makeFilterByType('file'))
+            .filter(({ name }) =>
+              acceptedFileTypes.includes(fileExtension(name))
+            );
+          const dirs = data.filter(makeFilterByType('dir'));
+          Promise.all(dirs.map(dir => getContents(dir.path))).then(
+            dirContents => {
+              resolve(flatten(files.concat(dirContents)));
+            }
+          );
+        });
+    })
+);
 
 const downloadFile = mem(url => axios.get(url).then(({ data }) => data));
 
 const getRandomSnippet = () =>
-  getContents()
-    .then(files =>
-      downloadFile(files[Math.floor(Math.random() * files.length)].download_url)
-    )
-    .then(({ data }) => data);
+  getContents().then(files =>
+    downloadFile(files[Math.floor(Math.random() * files.length)].download_url)
+  );
 
 export { getRandomSnippet };
