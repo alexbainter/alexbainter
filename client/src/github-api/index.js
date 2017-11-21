c72872f3b96e8cccd3b42fbe0c0d7d3f13851cc2
metalex9 <alexbainter@gmail.com>
2017-11-20 22:30:00 -0600

    Memoize github calls.

--- client/src/github-api/index.js
+++ client/src/github-api/index.js
@@ -1,12 +1,13 @@
 import fileExtension from 'file-extension';
 import axios from 'axios';
 import flatten from 'array-flatten';
+import mem from 'mem';
 
 const acceptedFileTypes = ['js', 'html', 'scss'];
 
 const makeFilterByType = match => ({ type }) => type === match;
 
-async function getContents(path = '') {
+const getContents = mem(async (path = '') => {
   const { data } = await axios.get(
     `https://api.github.com/repos/metalex9/alexbainter/contents${path}`
   );
@@ -16,12 +17,14 @@ async function getContents(path = '') {
   const dirs = data.filter(makeFilterByType('dir'));
   const dirContents = await Promise.all(dirs.map(dir => getContents(dir.path)));
   return flatten(files.concat(dirContents));
-}
+});
+
+const downloadFile = mem(url => axios.get(url).then(({ data }) => data));
 
 const getRandomSnippet = () =>
   getContents()
     .then(files =>
-      axios.get(files[Math.floor(Math.random() * files.length)].download_url)
+      downloadFile(files[Math.floor(Math.random() * files.length)].download_url)
     )
     .then(({ data }) => data);
 
