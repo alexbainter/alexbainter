d68fc499299e1c001c2e4528b9216d5cc257ab46
metalex9 <alexbainter@gmail.com>
2018-09-15 12:29:56 -0500

    Add project blurb about generative music

--- src/data/projects.js
+++ src/data/projects.js
@@ -8,14 +8,14 @@ const projects = [
     description:
       "You're looking at it! This is a static site built with React and hosted on AWS. I wanted the site's design to be clean and minimal, but with something interesting to watch on the home page. I consider the site an ongoing project and I try to update it frequently. Since the original launch, I've reduced its recurring hosting costs by 90%.",
     startDate: new Date(2017, 1, 1),
-    skills: ['JavaScript', 'React', 'CSS3', 'Sass', 'Webpack', 'HTML5'],
+    skills: ['JavaScript', 'React', 'CSS3', 'Sass', 'Webpack', 'HTML5', 'ES6+'],
   },
   {
     name: 'Really Big Dictionary',
     linkURL: 'http://reallybigdictionary.com',
     codeURL: 'https://github.com/metalex9/big-dictionary',
     description:
-      "This is a tiny website that displays definitions for English words from a third-party API. Click the link and you'll find out why I called it \"really big.\" I had a fun time designing this even though there isn't much to it. The neat part about this site is every word in the definition is actually a link to the definition of itself. If this were a serious project I would have made some indication of this feature; instead, it's left for the user to discover. I look forward to continuing to make small sites like this where I can play with the design.",
+      "A tiny website that displays definitions for English words from a third-party API. Click the link and you'll find out why I called it \"really big.\" I had a fun time designing this even though there isn't much to it. The neat part about this site is every word in the definition is actually a link to the definition of itself. If this were a serious project I would have made some indication of this feature; instead, it's left for the user to discover. I look forward to continuing to make small sites like this where I can play with the design.",
     startDate: new Date(2017, 2, 1),
     skills: [
       'JavaScript',
@@ -27,14 +27,33 @@ const projects = [
       'Pug',
       'jQuery',
       'Express',
+      'ES6+',
     ],
   },
   {
     name: 'Taco Builder',
     codeURL: 'https://github.com/metalex9/taco-builder',
     description:
-      'This is just the frontend of a site which allows users to choose recipes from the Taco Fancy API. It was built in a short amount of time to demonstrate and practice building sites with React/Redux',
+      'A webapp which allows users to choose recipes from the Taco Fancy API. It was built in a short amount of time to demonstrate and practice building sites with React/Redux',
     startDate: new Date(2017, 4, 1),
+    skills: [
+      'JavaScript',
+      'CSS3',
+      'Sass',
+      'Webpack',
+      'HTML5',
+      'Express',
+      'React',
+      'Redux',
+      'ES6+',
+    ],
+  },
+  {
+    name: 'Generative Music',
+    linkURL: 'https://generativemusic.alexbainter.com',
+    codeURL: 'https://github.com/generative-music/site',
+    description: '',
+    startDate: new Date(2018, 4, 1),
     skills: [
       'JavaScript',
       'CSS3',
@@ -42,9 +61,9 @@ const projects = [
       'Node',
       'Webpack',
       'HTML5',
-      'Express',
       'React',
-      'Redux',
+      'ES6+',
+      'Web Audio API',
     ],
   },
 ];
