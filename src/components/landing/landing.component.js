5dd1531b99e08b460d764488b87f0139f271f25c
metalex9 <alexbainter@gmail.com>
2019-01-29 23:27:00 -0600

    Add landing page

---
+++ src/components/landing/landing.component.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from 'react';
+import colors from 'material-colors';
+import colorConvert from 'color-convert';
+import landingStyles from './landing.module.css';
+
+// https://stackoverflow.com/questions/3942878/how-to-decide-font-color-in-white-or-black-depending-on-background-color
+const isColorBgForWhiteText = colorHex => {
+  const rgb = colorConvert.hex.rgb(colorHex);
+  const [r, g, b] = rgb.map(x => {
+    const pct = x / 255;
+    return pct <= 0.03928 ? pct / 12.92 : ((pct + 0.055) / 1.055) ** 2.4;
+  });
+  const luminance = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+  return luminance <= 0.179;
+};
+
+const backgroundColors = Reflect.ownKeys(colors)
+  .filter(color => typeof colors[color] === 'object')
+  .reduce(
+    (colorHexes, color) =>
+      colorHexes.concat(
+        Reflect.ownKeys(colors[color])
+          .filter(
+            value =>
+              !value.startsWith('a') && colors[color][value].startsWith('#')
+          )
+          .map(value => colors[color][value])
+      ),
+    []
+  )
+  .filter(colorHex => isColorBgForWhiteText(colorHex));
+
+const getRandomColor = previousColor => {
+  const newColors =
+    typeof previousColor === 'string'
+      ? backgroundColors.filter(color => color !== previousColor)
+      : backgroundColors;
+  return newColors[Math.floor(Math.random() * newColors.length)];
+};
+
+const Landing = () => {
+  const [bgColor, setBgColor] = useState(getRandomColor());
+  useEffect(() => {
+    setTimeout(() => {
+      setBgColor(currentBgColor => getRandomColor(currentBgColor));
+      setInterval(() => {
+        setBgColor(currentBgColor => getRandomColor(currentBgColor));
+      }, 15000);
+    }, 5000);
+  }, []);
+  return (
+    <div
+      className={landingStyles.container}
+      style={{ backgroundColor: bgColor }}
+    >
+      <div className={landingStyles.content}>
+        <h1 className={landingStyles.fontWeightNormal}>Alex Bainter</h1>
+        <h3 className={landingStyles.fontWeightNormal}>
+          Hi, I'm Alex. I'm a web developer who likes to create audio/visual
+          experiences both digital and not.
+        </h3>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,13 @@
-import React from 'react'
-import { Link } from 'gatsby'
-
-import Layout from '../components/layout'
-import Image from '../components/image'
-import SEO from '../components/seo'
+import React from 'react';
+import { Link } from 'gatsby';
+import SEO from '../components/seo';
+import Landing from '../components/landing/landing.component';
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+  <div>
+    <SEO title="Hello" keywords={['alex bainter']} />
+    <Landing />
+  </div>
+);
 
-export default IndexPage
+export default IndexPage;
