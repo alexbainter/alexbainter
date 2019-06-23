b0f9ff6f0b8c82be51189d65dd8159c0d9ec6273
metalex9 <alexbainter@gmail.com>
2019-06-23 13:54:00 -0500

    Add new screenshots of generative.fm

--- src/data/projects/generative-fm.js
+++ src/data/projects/generative-fm.js
@@ -8,7 +8,7 @@ module.exports = {
     In addition to the site itself, I create and maintain the generative music systems
     currently featured on it. Every performance is unique and never repeats, and yet the music lasts as long
     as you're willing to listen. Even though you'll never hear the same thing twice,
-    each performance of a piece is unmistakably familiar. I find the music is perfect for listening to during activities which
+    each performance of a piece is unmistakably familiar. Many users find the music is perfect for listening to during activities which
     require concentration, like reading, writing, or working.`,
   order: 1,
 };
