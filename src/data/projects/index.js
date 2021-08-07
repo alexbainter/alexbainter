1e655fb73e85f5a538b799f8f87797ef965c18cd
metalex9 <alexbainter@gmail.com>
2021-08-06 22:28:05 -0500

    Radically simplify the site

--- src/data/projects/index.js
+++
@@ -1,9 +0,0 @@
-const blossom = require('./blossom');
-const generativeMusic = require('./generative-fm');
-const corruptionLoops = require('./corruption-loops');
-
-const ORDER_PROP_NAME = 'order';
-
-module.exports = [blossom, generativeMusic, corruptionLoops].sort(
-  (a, b) => a[ORDER_PROP_NAME] - b[ORDER_PROP_NAME]
-);
